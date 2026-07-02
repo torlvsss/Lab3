eda8b01cb4d794afa39051ffcd12d86b3c8a7340
Tor <6711130029@mut.ac.th>
2026-07-02 16:36:16 +0700

    commit4

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
     echo "Hello Dit Hub";
     echo "My name is Tor";
-    echo "6711130029";
+    echo "029";
 ?>
---
+++ product.php
@@ -0,0 +1,5 @@
+<?php
+    echo "ink";
+    echo "jan";
+    echo "monday";
+?>
